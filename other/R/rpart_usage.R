665aab3c1de5fc1b89d94754fa8710e93f047c25
jayamathew <jayamathew@hotmail.com>
2018-03-01 15:45:58 -0500

    rpart usage
    code sample

---
+++ other/R/rpart_usage.R
@@ -0,0 +1,13 @@
+library(rpart)
+
+# grow tree 
+fit <- rpart(Kyphosis ~ Age + Number + Start, method="class", data=kyphosis)
+
+printcp(fit) # display the results 
+plotcp(fit) # visualize cross-validation results 
+summary(fit) # detailed summary of splits
+
+# plot tree 
+plot(fit, uniform=TRUE, 
+     main="Classification Tree for Kyphosis")
+text(fit, use.n=TRUE, all=TRUE, cex=.8)
