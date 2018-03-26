4ded688ddc02ce24dbaa04186d69f1c0b738edba
jayamathew <jayamathew@hotmail.com>
2018-03-26 14:13:02 -0400

    List with specific values
    value repeats at specified frequency

---
+++ python/createlist_specificvalues.py
@@ -0,0 +1,7 @@
+#create a list with 2 values [0,1] that occur a fixed number of times
+num_item1 = 10
+num_item2 = 2
+
+#new list contains only 2 items 0/1 which repeats 10/2 times respectively 
+new_list = [0] * num_item1 + [1] * num_item2
+len(new_list)
