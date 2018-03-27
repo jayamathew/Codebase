6982b526fb0190b00fa6271a3a94d3d96888827b
jayamathew <jayamathew@hotmail.com>
2018-03-27 12:11:53 -0400

    Sample code to download image from the web
    using urllib.request

---
+++ python/download_images_web.py
@@ -0,0 +1,6 @@
+#sample code to download an image from the web
+#repo of sample images to test: https://homepages.cae.wisc.edu/~ece533/images/
+
+import urllib.request
+
+urllib.request.urlretrieve('https://homepages.cae.wisc.edu/~ece533/images/airplane.png')
