ad1a552ff434c4948230b4a5185f3511e762b27c
jayamathew <jayamathew@hotmail.com>
2018-03-27 14:23:48 -0400

    Download images from web
    added in location to save the final image

--- python/download_images_web.py
+++ python/download_images_web.py
@@ -2,5 +2,10 @@
 #repo of sample images to test: https://homepages.cae.wisc.edu/~ece533/images/
 
 import urllib.request
+import os
 
-urllib.request.urlretrieve('https://homepages.cae.wisc.edu/~ece533/images/airplane.png')
+#add in the url for the image you want to download
+img = 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png'
+
+#retrieve and save the image to a specified location 
+urllib.request.urlretrieve(img, r'C:\Users\...\test' +os.path.basename(img))
